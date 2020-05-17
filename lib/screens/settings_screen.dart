c33d1a629dfbb4995ea287e4bea66ad0092782eb
David Conlisk <david@web-garden.co.uk>
2020-05-17 17:34:07 +0100

    (WIP) Added blood stool check text and button to settings page

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -4,8 +4,15 @@ import './dashboard_screen.dart';
 import '../widgets/main_drawer.dart';
 import '../providers/event_provider.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   static const routeName = '/settings';
+
+  @override
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  Future<void> _enableBlood(BuildContext context) async {}
 
   Future<void> _delete(BuildContext context) async {
     var provider = EventProvider();
@@ -33,24 +40,27 @@ class SettingsScreen extends StatelessWidget {
                   await provider.deleteAll();
                   Navigator.of(context).pop();
                   showDialog(
-                      context: context,
-                      builder: (ctx) {
-                        return AlertDialog(
-                            title: Text('Data deleted'),
-                            content: Text(
-                                'All of your data has been removed permanently.'),
-                            actions: <Widget>[
-                              FlatButton(
-                                child: Text('OK'),
-                                onPressed: () async {
-                                  await Navigator.pushNamedAndRemoveUntil(
-                                      context,
-                                      DashboardScreen.routeName,
-                                      (_) => false);
-                                },
-                              ),
-                            ]);
-                      });
+                    context: context,
+                    builder: (ctx) {
+                      return AlertDialog(
+                        title: Text('Data deleted'),
+                        content: Text(
+                            'All of your data has been removed permanently.'),
+                        actions: <Widget>[
+                          FlatButton(
+                            child: Text('OK'),
+                            onPressed: () async {
+                              await Navigator.pushNamedAndRemoveUntil(
+                                context,
+                                DashboardScreen.routeName,
+                                (_) => false,
+                              );
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
                 },
               )
             ],
@@ -69,6 +79,31 @@ class SettingsScreen extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: <Widget>[
+            RichText(
+              text: TextSpan(
+                style: Theme.of(context).textTheme.bodyText2,
+                children: <TextSpan>[
+                  TextSpan(
+                    text: 'Enable blood check',
+                    style: Theme.of(context).textTheme.headline3,
+                  ),
+                  TextSpan(
+                    text:
+                        '\r\n\r\nYou can record whenever you have blood in your stool by checking the box below. This will add a "blood in stool?" checkbox to the add screen. Any stools that have blood in them will '
+                        'be rendered in red on the graph.\r\n\r\n',
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 24),
+              child: FlatButton(
+                child: Text('Enable Blood In Stool Checkbox'),
+                color: Theme.of(context).primaryColor,
+                textColor: Colors.white,
+                onPressed: () => _enableBlood(context),
+              ),
+            ),
             RichText(
               text: TextSpan(
                 style: Theme.of(context).textTheme.bodyText2,
