a3276e0fa7df82c8152e169e65d33affb17b6dbc
David Conlisk <david@web-garden.co.uk>
2020-05-17 14:44:22 +0100

    Work on the graph

--- lib/widgets/graph.dart
+++ lib/widgets/graph.dart
@@ -13,122 +13,155 @@ class Graph extends StatefulWidget {
 }
 
 class _GraphState extends State<Graph> {
+  final smileyTextStyle = ChartTextStyle(
+    fontFamily: 'FontAwesome5Free',
+    color: Colors.black,
+    fontSize: 16,
+  );
+
   @override
   Widget build(BuildContext context) {
+    var fours = new List<Event>();
+    fours.add(new Event(
+      dateTime: widget.events[0].dateTime,
+      type: 4,
+    ));
+    fours.add(
+      new Event(
+        dateTime: widget.events[widget.events.length - 1].dateTime.add(Duration(
+          days: 2,
+        )),
+        type: 4,
+      ),
+    );
+
     return SfCartesianChart(
-        onTooltipRender: (args) => {args.header = "BSC"},
-        primaryXAxis: DateTimeAxis(
-          rangePadding: ChartRangePadding.none,
-          majorTickLines: MajorTickLines(color: Colors.white,),
-          minorGridLines: MinorGridLines(color: Colors.white,),
-          axisLine: AxisLine(
-            // color: Colors.white,
-          ),
-          desiredIntervals: 5,
-          dateFormat: DateFormat('MMM dd'),
-          intervalType: DateTimeIntervalType.days,
+      onTooltipRender: (args) => {args.header = "BSC"},
+      margin: EdgeInsets.all(16),
+      zoomPanBehavior: ZoomPanBehavior(
+        enablePanning: true,
+        enablePinching: true,
+        enableDoubleTapZooming: true,
+        zoomMode: ZoomMode.x,
+      ),
+      title: ChartTitle(
+        text: 'STOOL QUALITY',
+        alignment: ChartAlignment.near,
+        textStyle: ChartTextStyle(
+          fontSize: 16,
         ),
-        primaryYAxis: NumericAxis(
-          minimum: 1,
-          maximum: 7,
-          interval: 1,
-          majorTickLines: MajorTickLines(color: Colors.white,),
-          axisLine: AxisLine(
+      ),
+      tooltipBehavior: TooltipBehavior(enable: true),
+      primaryXAxis: DateTimeAxis(
+        rangePadding: ChartRangePadding.none,
+        majorTickLines: MajorTickLines(
+          color: Colors.white,
+        ),
+        minorGridLines: MinorGridLines(
+          color: Colors.white,
+        ),
+        majorGridLines: MajorGridLines(
+          width: 0,
+        ),
+        axisLine: AxisLine(
+          width: 0,
+        ),
+        desiredIntervals: 6,
+        dateFormat: DateFormat('MMM\ndd'),
+        intervalType: DateTimeIntervalType.days,
+      ),
+      primaryYAxis: NumericAxis(
+        minimum: 1,
+        maximum: 7,
+        interval: 1,
+        majorTickLines: MajorTickLines(
+          color: Colors.white,
+        ),
+        axisLine: AxisLine(
+          color: Colors.white,
+        ),
+        rangePadding: ChartRangePadding.additional,
+        plotBands: [
+          PlotBand(
+            start: 1,
+            end: 2,
             color: Colors.white,
+            opacity: 0.2,
+            text: '\u{f119}',
+            shouldRenderAboveSeries: true,
+            horizontalTextAlignment: TextAnchor.end,
+            verticalTextAlignment: TextAnchor.end,
+            textStyle: smileyTextStyle,
           ),
-          rangePadding: ChartRangePadding.none,
-          plotBands: [
-            PlotBand(
-              start: 1,
-              end: 2,
-              color: Colors.white,
-              opacity: 0.2,
-              text: '\u{f119}',
-              horizontalTextAlignment: TextAnchor.end,
-              textStyle: ChartTextStyle(
-                fontFamily: 'FontAwesome5Free',
-                color: Colors.black,
-              ),
-            ),
-            PlotBand(
-              start: 2,
-              end: 3,
-              color: Colors.white,
-              opacity: 0.2,
-              text: '\u{f11a}',
-              horizontalTextAlignment: TextAnchor.end,
-              textStyle: ChartTextStyle(
-                fontFamily: 'FontAwesome5Free',
-                color: Colors.black,
-              ),
-            ),
-            PlotBand(
-              start: 3,
-              end: 5,
-              color: Colors.white,
-              opacity: 0.2,
-              text: '\u{f118}',
-              horizontalTextAlignment: TextAnchor.end,
-              textStyle: ChartTextStyle(
-                fontFamily: 'FontAwesome5Free',
-                color: Colors.black,
-              ),
-            ),
-            PlotBand(
-              start: 5,
-              end: 6,
-              color: Colors.white,
-              opacity: 0.2,
-              text: '\u{f11a}',
-              horizontalTextAlignment: TextAnchor.end,
-              textStyle: ChartTextStyle(
-                fontFamily: 'FontAwesome5Free',
-                color: Colors.black,
-              ),
-            ),
-            PlotBand(
-              start: 6,
-              end: 7,
-              color: Colors.white,
-              opacity: 0.2,
-              text: '\u{f119}',
-              horizontalTextAlignment: TextAnchor.end,
-              textStyle: ChartTextStyle(
-                fontFamily: 'FontAwesome5Free',
-                color: Colors.black,
-              ),
-            ),
-          ],
+          PlotBand(
+            start: 2,
+            end: 3,
+            color: Colors.white,
+            opacity: 0.2,
+            text: '\u{f11a}',
+            shouldRenderAboveSeries: true,
+            horizontalTextAlignment: TextAnchor.end,
+            verticalTextAlignment: TextAnchor.end,
+            textStyle: smileyTextStyle,
+          ),
+          PlotBand(
+            start: 3,
+            end: 5,
+            color: Colors.white,
+            opacity: 0.2,
+            text: '\u{f118}',
+            horizontalTextAlignment: TextAnchor.end,
+            verticalTextAlignment: TextAnchor.middle,
+            textStyle: smileyTextStyle,
+          ),
+          PlotBand(
+            start: 5,
+            end: 6,
+            color: Colors.white,
+            opacity: 0.2,
+            text: '\u{f11a}',
+            horizontalTextAlignment: TextAnchor.end,
+            verticalTextAlignment: TextAnchor.middle,
+            textStyle: smileyTextStyle,
+          ),
+          PlotBand(
+            start: 6,
+            end: 7,
+            color: Colors.white,
+            opacity: 0.2,
+            text: '\u{f119}',
+            horizontalTextAlignment: TextAnchor.end,
+            verticalTextAlignment: TextAnchor.start,
+            textStyle: smileyTextStyle,
+          ),
+        ],
+      ),
+      series: <ChartSeries>[
+        FastLineSeries<Event, DateTime>(
+          color: Colors.black,
+          dataSource: widget.events,
+          xValueMapper: (Event event, int index) => event.dateTime,
+          yValueMapper: (Event event, int index) => event.type,
+          markerSettings: MarkerSettings(
+            isVisible: true,
+            borderColor: Colors.black,
+            borderWidth: 2,
+            color: Colors.white,
+            height: 10,
+            width: 10,
+          ),
+          dataLabelSettings: DataLabelSettings(isVisible: false),
         ),
-        zoomPanBehavior: ZoomPanBehavior(
-          enablePanning: true,
-          enablePinching: true,
-          zoomMode: ZoomMode.x,
-        ),
-        title: ChartTitle(
-          text: 'STOOL QUALITY',
-          alignment: ChartAlignment.near,
-          textStyle: ChartTextStyle(
-            fontSize: 16,
+        FastLineSeries<Event, DateTime>(
+          color: Colors.green,
+          dataSource: fours,
+          xValueMapper: (Event event, int index) => event.dateTime,
+          yValueMapper: (Event event, int index) => event.type,
+          markerSettings: MarkerSettings(
+            isVisible: false,
           ),
         ),
-        tooltipBehavior: TooltipBehavior(enable: true),
-        series: <ChartSeries>[
-          FastLineSeries<Event, DateTime>(
-            color: Colors.black,
-            dataSource: widget.events,
-            xValueMapper: (Event event, int index) => event.dateTime,
-            yValueMapper: (Event event, int index) => event.type,
-            markerSettings: MarkerSettings(
-              isVisible: true,
-              borderColor: Colors.black,
-              borderWidth: 2,
-              color: Colors.white,
-              height: 10,
-              width: 10,
-            ),
-            dataLabelSettings: DataLabelSettings(isVisible: false),
-          )
-        ]);
+      ],
+    );
   }
 }
