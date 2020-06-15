933034637537436671988e290a4d7e76d2f48306
David Conlisk <david@web-garden.co.uk>
2020-06-15 14:21:08 +0100

    Added write bytes to disk method to file helper

--- lib/helpers/file_helper.dart
+++ lib/helpers/file_helper.dart
@@ -1,16 +1,30 @@
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:intl/intl.dart';
 
 class FileHelper {
-    static Future<String> writeCsvToFile(BuildContext context, String csvData) async {
+  static Future<String> writeStringToFile(
+      BuildContext context, String data) async {
     final directory = await getTemporaryDirectory();
-    var attachmentFilename = 'BristolStoolChartData_${DateFormat('yyyyMMddHHmm').format(DateTime.now())}.csv';
+    var attachmentFilename =
+        'BristolStoolChartData_${DateFormat('yyyyMMddHHmm').format(DateTime.now())}.csv';
     final csvFile = File('${directory.path}/$attachmentFilename');
     print(csvFile.path);
-    await csvFile.writeAsString(csvData);
+    await csvFile.writeAsString(data);
     return csvFile.path;
-    }
+  }
+
+  static Future<String> writeBytesToFile(
+      BuildContext context, Uint8List bytes) async {
+    final directory = await getTemporaryDirectory();
+    var attachmentFilename =
+        'BristolStoolChartGraph_${DateFormat('yyyyMMddHHmm').format(DateTime.now())}.png';
+    final csvFile = File('${directory.path}/$attachmentFilename');
+    print(csvFile.path);
+    await csvFile.writeAsBytes(bytes);
+    return csvFile.path;
+  }
 }
