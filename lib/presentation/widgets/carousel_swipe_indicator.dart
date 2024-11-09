635b9619333143827dd28a5a86c109309ecfaabc
David Conlisk <david@web-garden.co.uk>
2024-11-09 12:13:43 +0000

    Made swipe indicator appear only once

--- lib/presentation/widgets/carousel_swipe_indicator.dart
+++ lib/presentation/widgets/carousel_swipe_indicator.dart
@@ -6,6 +6,8 @@ class CarouselSwipeIndicator extends StatefulWidget {
   final Duration showDuration;
   final Duration animationDuration;
 
+  static bool _hasShownOnce = false;
+
   const CarouselSwipeIndicator({
     super.key,
     this.width = 40.0,
@@ -13,6 +15,10 @@ class CarouselSwipeIndicator extends StatefulWidget {
     this.showDuration = const Duration(seconds: 2),
     this.animationDuration = const Duration(milliseconds: 1000),
   });
+
+  static void reset() {
+    _hasShownOnce = false;
+  }
 
   @override
   State<CarouselSwipeIndicator> createState() => _CarouselSwipeIndicatorState();
@@ -28,13 +34,21 @@ class _CarouselSwipeIndicatorState extends State<CarouselSwipeIndicator>
   void initState() {
     super.initState();
 
-    // Initialize animation controller
+    // If already shown once, start hidden
+    if (CarouselSwipeIndicator._hasShownOnce) {
+      _visible = false;
+    } else {
+      CarouselSwipeIndicator._hasShownOnce = true;
+      _initializeAnimation();
+    }
+  }
+
+  void _initializeAnimation() {
     _controller = AnimationController(
       duration: widget.animationDuration,
       vsync: this,
     );
 
-    // Create sliding animation
     _slideAnimation = Tween<Offset>(
       begin: const Offset(-0.2, 0.0),
       end: const Offset(0.2, 0.0),
@@ -43,27 +57,35 @@ class _CarouselSwipeIndicatorState extends State<CarouselSwipeIndicator>
       curve: Curves.easeInOut,
     ));
 
-    // Start the animation
     _controller.repeat(reverse: true);
 
-    // Hide the indicator after showDuration
     Future.delayed(widget.showDuration, () {
       if (mounted) {
         setState(() {
           _visible = false;
         });
+        // Stop and dispose the controller after the animation is hidden
+        _controller.stop();
+        _controller.dispose();
       }
     });
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    // Only dispose if the controller was actually initialized
+    if (!CarouselSwipeIndicator._hasShownOnce || _visible) {
+      _controller.dispose();
+    }
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    if (CarouselSwipeIndicator._hasShownOnce && !_visible) {
+      return const SizedBox.shrink();
+    }
+
     return AnimatedOpacity(
       opacity: _visible ? 1.0 : 0.0,
       duration: const Duration(milliseconds: 500),
